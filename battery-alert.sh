bb3f444904a0fadb247ec401f119b97abf67f581
Diljith A K <diljithak07@gmail.com>
2025-08-04 21:18:33 +0530

    Update triggering condition to show notification if battery less than 30%

--- battery-alert.sh
+++ battery-alert.sh
@@ -3,6 +3,6 @@
 # Get battery percentage
 battery_level=$(cat /sys/class/power_supply/BAT0/capacity)
 
-if [ "$battery_level" -lt 70 ]; then
+if [ "$battery_level" -lt 30 ]; then
 	notify-send --icon=battery-caution "Low Battery" "Battery is at ${battery_level}%! Please plug in the charger." -u critical
 fi

6549a5c50299e5badbd34021d1a92082fabca30e
Diljith A K <diljithak07@gmail.com>
2025-08-04 21:14:28 +0530

    Updated battery-alert to show notification

--- battery-alert.sh
+++ battery-alert.sh
@@ -1,1 +1,8 @@
 #!/bin/env bash
+
+# Get battery percentage
+battery_level=$(cat /sys/class/power_supply/BAT0/capacity)
+
+if [ "$battery_level" -lt 70 ]; then
+	notify-send --icon=battery-caution "Low Battery" "Battery is at ${battery_level}%! Please plug in the charger." -u critical
+fi
